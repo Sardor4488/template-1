1aba313f60035add31af8625dae5a5d61cb2f6dc
sardor4488 <sardortemirov84@gmail.com>
2021-12-22 21:50:13 +0500

    admin panel/ translate blog sections

--- src/Admin/MainPage/Pages/Blog/editblog.jsx
+++ src/Admin/MainPage/Pages/Blog/editblog.jsx
@@ -1,20 +1,21 @@
-
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class EditBlog extends Component {
-   render() {
-      return ( 
-        <div className="page-wrapper">
+  render() {
+    return (
+      <div className="page-wrapper">
         <div className="content container-fluid">
           {/* Page Header */}
           <div className="page-header">
             <div className="row">
               <div className="col-sm-12">
-                <h3 className="page-title">Edit Blog</h3>
+                <h3 className="page-title"> Blogni tahrirlash</h3>
                 <ul className="breadcrumb">
-                  <li className="breadcrumb-item"><Link to="/admin/index">Dashboard</Link></li>
-                  <li className="breadcrumb-item active">Edit Blog</li>
+                  <li className="breadcrumb-item">
+                    <Link to="/admin/index">Dashboard</Link>
+                  </li>
+                  <li className="breadcrumb-item active"> Blogni tahrirlash</li>
                 </ul>
               </div>
             </div>
@@ -29,21 +30,29 @@ class EditBlog extends Component {
                     <div className="col-12 blog-details">
                       <form>
                         <div className="form-group">
-                          <label>Blog Name</label>
+                          <label>Blog nomi</label>
                           <input className="form-control" type="text" />
                         </div>
                         <div className="form-group">
-                          <label>Blog Images</label>
+                          <label>Blog rasmi</label>
                           <div>
                             <input className="form-control" type="file" />
-                            <small className="form-text text-muted">Max. file size: 50 MB. Allowed images: jpg, gif, png. Maximum 10 images only.</small>
+                            <small className="form-text text-muted">
+                              Fayl hajmi 50mb dan oshmasligi kerak Rasm
+                              formatlari: jpg, gif, png. Rasm soni 10 tadan
+                              oshmasin.
+                            </small>
                           </div>
                         </div>
                         <div className="row">
                           <div className="col-md-6">
                             <div className="form-group">
-                              <label>Blog Category</label>
-                              <select className="select select2-hidden-accessible form-control" tabIndex={-1} aria-hidden="true">
+                              <label>Blog kategoryalari</label>
+                              <select
+                                className="select select2-hidden-accessible form-control"
+                                tabIndex={-1}
+                                aria-hidden="true"
+                              >
                                 <option>Web Design</option>
                                 <option>Web Development</option>
                                 <option>App Development</option>
@@ -52,8 +61,12 @@ class EditBlog extends Component {
                           </div>
                           <div className="col-md-6">
                             <div className="form-group">
-                              <label>Blog Sub Category</label>
-                              <select className="select select2-hidden-accessible form-control" tabIndex={-1} aria-hidden="true">
+                              <label>Blog sub kategoryalari</label>
+                              <select
+                                className="select select2-hidden-accessible form-control"
+                                tabIndex={-1}
+                                aria-hidden="true"
+                              >
                                 <option>Html</option>
                                 <option>Css</option>
                                 <option>Javascript</option>
@@ -66,24 +79,56 @@ class EditBlog extends Component {
                           </div>
                         </div>
                         <div className="form-group">
-                          <label>Blog Description</label>
-                          <textarea cols={30} rows={6} className="form-control" defaultValue={""} />
+                          <label>Blog Matni</label>
+                          <textarea
+                            cols={30}
+                            rows={6}
+                            className="form-control"
+                            defaultValue={""}
+                          />
                         </div>
                         <div className="form-group">
-                          <label className="display-block w-100">Blog Status</label>
+                          <label className="display-block w-100">
+                            Blog holati
+                          </label>
                           <div>
                             <div className="custom-control custom-radio custom-control-inline">
-                              <input className="custom-control-input" id="active" name="active-blog" defaultValue="active" type="radio" defaultChecked />
-                              <label className="custom-control-label" htmlFor="active">Active</label>
+                              <input
+                                className="custom-control-input"
+                                id="active"
+                                name="active-blog"
+                                defaultValue="active"
+                                type="radio"
+                                defaultChecked
+                              />
+                              <label
+                                className="custom-control-label"
+                                htmlFor="active"
+                              >
+                                Faol
+                              </label>
                             </div>
                             <div className="custom-control custom-radio custom-control-inline">
-                              <input className="custom-control-input" id="inactive" name="active-blog" defaultValue="inactive" type="radio" />
-                              <label className="custom-control-label" htmlFor="inactive">Inactive</label>
+                              <input
+                                className="custom-control-input"
+                                id="inactive"
+                                name="active-blog"
+                                defaultValue="inactive"
+                                type="radio"
+                              />
+                              <label
+                                className="custom-control-label"
+                                htmlFor="inactive"
+                              >
+                                Nofaol
+                              </label>
                             </div>
                           </div>
                         </div>
                         <div className="m-t-20 text-center">
-                          <button className="btn btn-primary btn-lg">Save Changes</button>
+                          <button className="btn btn-primary btn-lg">
+                            Tasdiqlash
+                          </button>
                         </div>
                       </form>
                     </div>
@@ -91,12 +136,12 @@ class EditBlog extends Component {
                   {/* /Add details */}
                 </div>
               </div>
-            </div>			
+            </div>
           </div>
-        </div>			
+        </div>
       </div>
-      );
-   }
+    );
+  }
 }
 
 export default EditBlog;
